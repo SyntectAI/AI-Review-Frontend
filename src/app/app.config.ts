9cc207869e5174e0a511d70f0eba1f9054faab71
Aleksey <AlekseyVY@gmail.com>
2025-10-18 22:48:51 +0300

    initial deps

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,8 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+  providers: [provideBrowserGlobalErrorListeners(), provideZonelessChangeDetection(), provideRouter(routes), provideStore()],
 };
